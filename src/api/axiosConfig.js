a82109b1331948d9ea86112313604ae74c99a77b
Shivendra2129 <pagadalashivendra21@gmail.com>
2025-10-18 16:01:22 +0530

    fourth

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://backend-zo5n.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+   baseURL: "https://backend-zo5n.onrender.com/api",
+  //baseURL: "http://localhost:5000/api",
 
 });
 
